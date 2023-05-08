4c9acb50d2e10a673f114295a863eb4d6b97c784
Csongor Bodnar <csongor.bodnar@silabs.com>
2023-05-08 20:38:41 +0200

    marked on the way

--- client/src/views/Marked.vue
+++ client/src/views/Marked.vue
@@ -5,9 +5,21 @@
 import store from "./../store";
 export default {
   name: "MarkedFlight",
+  data() {
+    return {
+      places: [],
+      agents: [],
+      carriers: [],
+      pricingOptions: [],
+      legIds: [],
+      segments: [],
+    };
+  },
   created() {
-    store.dispatch("fetchMarkedFlightData");
-    console.log(this.marked);
+    store.dispatch("fetchMarkedFlightData").then((response) => {
+      console.log(response.data);
+    });
+    // console.log(this.marked);
   },
   computed: {
     marked() {
